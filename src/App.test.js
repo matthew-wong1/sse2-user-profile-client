8ec12016b32f695740ff359d48c9e887180664f3
Matthew Wong <matthew.wong1@outlook.com>
2024-02-12 21:16:16 +0000

    disable tests temporarily

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+jest.mock('./App')
+jest.mock('axios');
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Test for the /signup route
+test('renders the signup form', () => {
+  render(
+    <MemoryRouter initialEntries={['/signup']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/password/i)).toBeInTheDocument();
+});
+
+// Test for the /user-profile route
+test('renders the user profile', () => {
+  render(
+    <MemoryRouter initialEntries={['/user-profile']}>
+      <App />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/User Information/i)).toBeInTheDocument();
 });

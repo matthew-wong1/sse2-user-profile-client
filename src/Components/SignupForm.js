633e13c638cd74e46531efcc8865a373f90463a2
Matthew Wong <matthew.wong1@outlook.com>
2024-02-15 15:36:22 +0000

    add additional form fields

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -10,7 +10,6 @@ function SignupForm() {
 		email: "",
 		password: "",
 		rePassword: "",
-		degree: "",
 	});
 
 	// Setup errors
@@ -20,19 +19,34 @@ function SignupForm() {
 	const [startYear, setStartYear] = useState("");
 	const [endYear, setEndYear] = useState("");
 	const currentYear = new Date().getFullYear();
+	// 10 years ago to this year
 	const startYears = Array.from(
 		{ length: 11 },
 		(val, index) => currentYear - 10 + index
-	); // 10 years ago to this year
+	);
+	// This year to 10 years after
 	const endYears = Array.from(
 		{ length: 11 },
 		(val, index) => currentYear + index
-	); // This year to 10 years after
+	);
+
+	// Degree levels
+	const [degree, setDegree] = useState("");
+	const [degreeLevel, setDegreeLevel] = useState("");
 
 	// Setup checkboxes
 	const [international, setInternational] = useState(false);
 
 	const navigate = useNavigate();
+
+	// Handle changes to form
+	const handleDegreeChange = (event) => {
+		setDegree(event.target.value);
+	};
+
+	const handleDegreeLevelChange = (event) => {
+		setDegreeLevel(event.target.value);
+	};
 
 	const handleCheckboxChange = (event) => {
 		setInternational(event.target.checked);
@@ -51,18 +65,38 @@ function SignupForm() {
 		setUser((prevUser) => ({ ...prevUser, [name]: value }));
 	};
 
+	// Form validation
 	const validate = (user) => {
 		let errors = {};
-		if (!user.firstName.trim()) errors.firstName = "First name is required";
-		if (!user.email) errors.email = "Email is required";
-		else if (!/\S+@\S+\.\S+/.test(user.email))
-			errors.email = "Email address is invalid";
-		if (!user.password) errors.password = "Password is required";
-		else if (user.password.length < 6)
-			errors.password = "Password must be 6 characters or more";
+
+		// Every field (except international cannot be empty)
+		// Check if any field in the user object is empty
+		Object.keys(user).forEach((key) => {
+			// Trim the value to check for spaces only entries as well
+			if (!user[key].trim()) {
+				errors[key] = "Field cannot be empty";
+			}
+		});
+
+		if (!errors) {
+			// University Email: Checks that has an @ followed by a dot
+			if (!/\S+@\S+\.\S+/.test(user.email))
+				errors.email = "Email address is invalid";
+
+			// Password: Must meet min requirements
+			if (user.password.length < 8)
+				errors.password = "Password must be 8 characters or more";
+			if (user.password !== user.rePassword)
+				errors.rePassword = "Passwords must match";
+		}
+		// Backend will query Hipo API
+
+		// Duplicate checks in backend
+
 		return errors;
 	};
 
+	// Setup submission
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 		const validationErrors = validate(user);
@@ -79,6 +113,7 @@ function SignupForm() {
 		}
 	};
 
+	// Render form
 	return (
 		<form onSubmit={handleSubmit}>
 			<div>
@@ -124,24 +159,47 @@ function SignupForm() {
 			<div>
 				<input
 					type="password"
-					name="repassword"
+					name="rePassword"
 					value={user.rePassword}
 					onChange={handleChange}
 					placeholder="Re-enter your password"
 				/>
 				{errors.rePassword && (
-					<p style={{ color: "red" }}>{errors.rePpassword}</p>
+					<p style={{ color: "red" }}>{errors.rePassword}</p>
 				)}
 			</div>
 			<div>
-				<input
-					type="text"
-					name="degree"
-					value={user.degree}
-					onChange={handleChange}
-					placeholder="Full name of degree"
-				/>
-				{errors.degree && <p style={{ color: "red" }}>{errors.degree}</p>}
+				<label>
+					Degree
+					<select value={degree} onChange={handleDegreeChange}>
+						<option value="" disabled selected>
+							Select a degree
+						</option>
+						<option value="1">Biology</option>
+						<option value="2">Business</option>
+						<option value="3">Chemistry</option>
+						<option value="4">Computer Science</option>
+						<option value="5">Engineering</option>
+						<option value="6">English</option>
+						<option value="7">History</option>
+						<option value="8">Law</option>
+						<option value="9">Medicine</option>
+						<option value="10">Physics</option>
+					</select>
+				</label>
+			</div>
+			<div>
+				<label>
+					Degree Level
+					<select value={degreeLevel} onChange={handleDegreeLevelChange}>
+						<option value="" disabled selected>
+							Select a degree level
+						</option>
+						<option value="1">Bachelor's</option>
+						<option value="2">Master's</option>
+						<option value="3">Doctorate</option>
+					</select>
+				</label>
 			</div>
 			<div>
 				<label>
@@ -158,7 +216,9 @@ function SignupForm() {
 				<label>
 					Start Year
 					<select value={startYear} onChange={handleStartYearChange}>
-						<option value="">Select a year</option>
+						<option value="" disabled selected>
+							Select a year
+						</option>
 						{startYears.map((year) => (
 							<option key={year} value={year}>
 								{year}
@@ -171,7 +231,9 @@ function SignupForm() {
 				<label>
 					End Year
 					<select value={endYear} onChange={handleEndYearChange}>
-						<option value="">Select a year</option>
+						<option value="" disabled selected>
+							Select a year
+						</option>
 						{endYears.map((year) => (
 							<option key={year} value={year}>
 								{year}
